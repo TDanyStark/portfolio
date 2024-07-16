f8ae26f2e5dee977af4e578a259732dde3834082
Daniel Amado <daniel.amadove@gmail.com>
2024-07-16 13:36:22 -0500

    agregar el header con efecto glass

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-      defaultLocale: "es",
-      locales: ["en", "es"],
-      routing: {
-        prefixDefaultLocale: false
+  i18n: {
+    defaultLocale: "es",
+    locales: ["en", "es"],
+    routing: {
+      prefixDefaultLocale: false
     }
-  }
+  },
+  integrations: [tailwind()]
 });
